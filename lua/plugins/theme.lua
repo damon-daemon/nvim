8fe4ed440fd357717885f68c8ed127147eada622
damon-daemon <17775525+damon-daemon@users.noreply.github.com>
2026-04-27 14:39:45 -0400

    Replace broken omarchy symlink with cross-platform theme loader

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,1 +1,18 @@
-/home/leto2/.config/omarchy/current/theme/neovim.lua
+local omarchy_theme = vim.fn.expand("~/.config/omarchy/current/theme/neovim.lua")
+if vim.fn.filereadable(omarchy_theme) == 1 then
+  return dofile(omarchy_theme)
+end
+
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
